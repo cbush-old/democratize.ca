09e3bd706aed880948bd7f407a13c9fcbca83389
crust <chrbsh@gmail.com>
2013-04-09 17:25:04 -0400

    config contains database access info

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
 
+//  Credentials for read-only access to the `dmc_public` database.
+//  Your IP is probably locked out. Contact me for access
+//  or download a copy of the database onto your local server
+//  and change this information accordingly.
+
 define("DB_DSN", "mysql:dbname=dmc_public;host=mysql.democratize.ca");
 define("DB_USER", "dmc_reader");
 define("DB_PASS", "Antidisestablishmentarian");
