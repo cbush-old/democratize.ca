c795a503aeabcf6c4ee276a09d3e2d3ae18753d0
crust <chrbsh@gmail.com>
2013-04-13 15:06:43 -0400

    database cleanup

--- public/api/index.php
+++ public/api/index.php
@@ -8,35 +8,23 @@ require_once("../../base/helpers.php");
 require_once("../../base/codes.php");
 require_once("../../base/db.php");
 
-require("../../base/request.php");
-require("../../request/bill.php");
-require("../../request/mp.php");
-require("../../request/comment.php");
-require("../../request/help.php");
-require("../../request/vote.php");
-require("../../request/user.php");
+require_once("../../base/request.php");
 
+static $action = array(
+  'bill' => 'Bill_request',
+  'comment' => 'Comment_request',
+  'help' => 'Help_request',
+  'mp' => 'Mp_request',
+  'summary' => 'Summary_request',
+  'user' => 'User_request',
+  'vote' => 'Vote_request',
+);
 
-function get_request($cmd, $args){
+//  load the request classes which handle the actions
 
-  static $action = array(
-    'bill' => 'Bill_request',
-    'mp' => 'Mp_request',
-    'comment' => 'Comment_request',
-    'help' => 'Help_request',
-    'vote' => 'Vote_request',
-    'user' => 'User_request'
-  );
+foreach(array_keys($action) as $a)
+  require("../../request/{$a}.php");
 
-  if(!isset($action[$cmd])) 
-    throw new HTTP_status(400);
-  
-  if(!class_exists($action[$cmd]))
-    throw new HTTP_status(500);
-  
-  return new $action[$cmd] (request_method(), $args);
-  
-}
 
 
 //  crunch the user data from QUERY_STRING:
@@ -45,18 +33,32 @@ foreach($_GET as $k => $v)
   $_GET[$k] = feels_good_man($v);
   
 
+
+
 try {
+
 
   if(!isset($_GET["uri"])) 
     $_GET["uri"] = "help";
 
+    
   $args = explode("/", $_GET["uri"]);
   
   $cmd = array_shift($args);
+
+  if(!isset($action[$cmd])) 
+    throw new HTTP_status(400);
   
-  $request = get_request($cmd, $args);
+  if(!class_exists($action[$cmd])) 
+    throw new HTTP_status(500);
   
+  
+  $method = request_method();
+  
+  $request = new $action[$cmd] ($method, $args);
+
   $Response = $request->get_response();
+
 
   preg_match("/^(html|json|xml)$/",$_GET["format"])
     and $format = $_GET["format"]
@@ -64,7 +66,7 @@ try {
 
   require "../../inc/out.{$format}.php";
 
-  
+
 } catch(HTTP_status $e){
   
   $_GET = array($e->code => $e->reason);
