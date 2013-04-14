58b9f85deea88241ac84d3789be7389179346b8f
crust <chrbsh@gmail.com>
2013-04-14 17:34:52 -0400

    xml implemented, more or less

--- inc/out.xml.php
+++ inc/out.xml.php
@@ -1,1 +1,48 @@
 <?php
+
+header("Content-type:text/xml");
+
+$doc = new DOMDocument('1.0', 'iso-8859-1');
+
+
+function xml_encode(&$node, $k, $v){
+  
+  global $doc;
+  
+  if(is_object($v)||is_array($v)){
+  
+    $n = $doc->createElement($k);
+    
+    foreach($v as $kk=>$vv){
+      if(is_numeric($kk)){
+        
+        $singular_k = substr($k,-1)=="s"?
+          substr($k,0,strlen($k)-1):"{$k}Inst";
+          
+        if(!strlen($singular_k)) $singular_k = "{$k}Inst";
+        
+        xml_encode($n, $singular_k, $vv);
+        
+      } else {
+      
+        xml_encode($n, $kk, $vv);
+        
+      }
+      
+    }
+  } else {
+  
+    $n = $doc->createElement($k, $v);
+  
+  }
+
+  $node->appendChild($n);
+  
+}
+
+
+xml_encode($doc, "Response", $Response);
+
+echo $doc->saveXML();
+
+
