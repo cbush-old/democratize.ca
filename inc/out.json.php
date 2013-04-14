2d8581770f8ad964375ab4d7bc96a117ceb65434
crust <chrbsh@gmail.com>
2013-04-14 16:20:15 -0400

    'callback' in query string on json requests returns application/javascript

--- inc/out.json.php
+++ inc/out.json.php
@@ -1,5 +1,19 @@
 <?php
-header("Content-type:application/json");
 
-echo json_encode($Response);
+$data = json_encode($Response);
 
+if(isset($_GET["callback"])){
+  
+  $callback = preg_replace("/[^0-9A-z_-]/","",$_GET["callback"]);
+  $data = "{$callback}({$data});";
+
+  header("Content-type:application/javascript");
+
+
+} else {
+
+  header("Content-type:application/json");
+
+}
+
+echo $data;
