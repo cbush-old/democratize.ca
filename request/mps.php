42247f4151761ae528001d387c875ba180982207
crust <chrbsh@gmail.com>
2013-04-14 01:55:37 -0400

    more api commands: mps, subjects

--- request/mps.php
+++ request/mps.php
@@ -3,37 +3,44 @@
 class Mps_request extends Request {
 
   public function GET($args){
-  
-    $name = array_shift($args);
     
-    $argstr = implode(" ",$args);
+    $select = array(
+      "mp.first_name",
+      "mp.last_name",
+      "mp.lang_pref",
+      "mp.party",
+      "party.name_en as party_en",
+      "party.name_fr as party_fr",
+      "riding.name as riding",
+      "riding.province as province",
+      "(select count(*) from bill 
+        where bill.mp_alias = alias_mp.alias 
+        && alias_mp.mp_lcname = mp.lcname
+      ) as n_bills"
+    );
+
+    $select = implode(",", $select);
     
-    if(!preg_match("/^[a-z-]+$/",$name))
-      throw new HTTP_status (400);
-      
-    $bill = get_bill_from_uri_string($argstr);
-    
-    $name = (int)dmchash($name);
-    
-    $query =
-      "select *
-      from bills
-      join bills_mps on bills.id = bills_mps.bill_id
-      join mps on mps.parl_id = bills_mps.sponsor_parl_id
-      left join ridings_mps on mps.id = ridings_mps.mp_id
-      left join ridings on ridings_mps.riding_id = ridings.id
-      where mps.id = {$name}
-      group by bills.id";
-      
-    
+    $query = "select {$select} from mp 
+      join party on party.id = mp.party
+      join riding on riding.lcname = mp.riding_lcname
+      join alias_mp on alias_mp.mp_lcname = mp.lcname
+      join bill on bill.mp_alias = alias_mp.alias
+      group by mp.lcname
+      ";
     $result = DB::query($query);
-  
-    var_dump(DB::get()->errorInfo());
+      
+    $this->response->n_results = $result->rowCount();
     
-    while($r = $result->fetchObject())
-      $this->response->bills[] = $r;
-  
-  
-  }
+    $this->response->mps = array();
+      
+    
+    while($r = $result->fetchObject()){
+      
+      $this->response->mps[] = $r;
+    
+    }
+
+    return;
 
 }
