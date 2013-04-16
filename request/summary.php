13f64b86ace168ed4ff3be5633b701a4159a5303
crust <chrbsh@gmail.com>
2013-04-16 16:56:15 -0400

    select summaries by bill

--- request/summary.php
+++ request/summary.php
@@ -3,12 +3,41 @@
 class Summary_request extends Request {
 
   public function GET($args){
-
-    $result = DB::query("select * from summary");
+  
+    $select = array(
+      "summary.pscn as bill",
+      "summary.summary_en",
+      "summary.summary_fr",
+    );
     
-    while($r = $result->fetchObject())
-      $this->response->summaries[] = $r;
-      
+    $select = implode(",",$select);
+    
+    $where = array();
+    
+    foreach($args as $arg){
+      if(preg_match("/^([1-9][0-9])(?:-?([1-9]))?$/", $arg, $match)){
+        if(isset($match[1])) $where[] = "bill.parl='{$match[1]}'";
+        if(isset($match[2])) $where[] = "bill.sess='{$match[2]}'";
+      } else if(preg_match("/^(c|s|t|u)-?([1-9][0-9]{0,4})?$/", $arg, $match)){
+        if(isset($match[1])) $where[] = "bill.chamber='".strtoupper($match[1])."'";
+        if(isset($match[2])) $where[] = "bill.number='{$match[2]}'";
+      }
+    }
+    
+    $where = implode("&&", $where);
+    if($where) $where = "where {$where}";
+    
+    $r = DB::query("select {$select} from bill
+      join summary on summary.pscn = bill.pscn
+      {$where} group by bill.pscn");
+    
+    $this->response->summaries = array();
+    
+    while($row = $r->fetchObject()){
+      $this->response->summaries[] = $row;
+    
+    }
+  
   }
-
+  
 }
