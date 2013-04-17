5f3c5e2295d2dc9df62eee44b3d8bc9d5e58085e
crust <chrbsh@gmail.com>
2013-04-17 19:02:41 -0400

    user creation, login

--- request/session.php
+++ request/session.php
@@ -8,33 +8,47 @@ class Session_request extends Request {
   
     // Authenticate the user, then set a session cookie.
     
-    if(count($args)!=1)
-      return "Argument count must be exactly 1 (username)";
-    
     $bcrypt = new bcrypt();
-    $user = DB::get()->quote(array_pop($args));
-    $hash = $bcrypt->hash($_POST["pass"]);
+    
+    $required = array("user","pass");
+    $missing = array();
+    
+    foreach($required as $k){
+      if(!isset($_POST[$k]))
+        $missing[] = $k;
+    }
+    
+    if(count($missing))
+      return array("missing"=>$missing);
+      
+    $value = DB::get()->quote($_POST['user']);
+    $key = strpos($value,'@') ? 'email':'username';
+    
+    $hash = DB::get()->quote($bcrypt->hash($_POST['pass']));
 
-    $r = DB::query("select username, hash from user where username={$user}");
+    $r = DB::query("select email, hash from user where {$key}={$value}");
     
     if($r->rowCount()==0)
-      return "Username not found";
+      return ucfirst($key)." not found";
     
     $row = $r->fetchObject();
     
-    if(!$bcrypt->verify($hash, $row->hash))
-      return "Password incorrect";
-    
-    $created = DB::get()->quote($created);
+    if(!$bcrypt->verify($_POST['pass'],$row->hash))
+      return "Password incorrect".$hash.$row->hash;
+      
+    $user = DB::get()->quote($row->email);
+    $created = DB::get()->quote(time());
     $session_id = DB::get()->quote(sha1($user.$created));
     
+    
     $values = implode(",",array($session_id,$user,$created));
+    
     
     try {
     
       DB::get(1)->query("start transaction;");
-      DB::get(1)->query("delete from session where username={$user};");
-      DB::get(1)->query("insert into session (id, username, created) values ({$values})");
+      DB::get(1)->query("delete from session where user={$user};");
+      DB::get(1)->query("insert into session (id, user, created) values ({$values})");
       DB::get(1)->query("commit;");
       setcookie("session", $session_id);
       $this->response->logged_in = true;
--- request/user.php
+++ request/user.php
@@ -6,19 +6,95 @@ require_once(__DIR__."/../base/bcrypt.php");
 class User_request extends Request {
 
   public function GET($args){
-
     
-  
   }
   
   public function POST($args){
     
+    $bcrypt = new bcrypt();
     
+    $missing = array();
+    $required = array("email","pass");
+    
+    foreach($required as $k){
+     
+      if(!isset($_POST[$k]))
+        $missing[] = $k;
+        
+    }
+    
+    if(count($missing))
+      return array("missing"=>$missing);
+    
+    if(!preg_match("/^[A-z0-9-_\.]+@[A-z0-9-_\.]+\.[a-z]{2,4}$/",$_POST["email"]))
+      return "Invalid e-mail address format";
+    
+    if(strlen($_POST["pass"]) < 6)
+      return "Password must be at least 6 characters long";
+    
+    $email = DB::get()->quote($_POST['email']);
+    
+    $r = DB::query("select email from user where email={$email}");
+    
+    if($r->rowCount())
+      return "E-mail already in use for another account";
+    
+    if(isset($_POST['riding'])){
+      $riding = DB::get()->quote($_POST['riding']);
+      $r = DB::query("select count(*) from riding where lcname={$riding}");
+      if(!$r->rowCount()){
+        $this->response->notice[] = "Riding {$riding} not found; ignored";
+        $riding = "";
+      }
+    } else {
+      $riding = "";
+    }
+    
+    
+    $values = array();
+    $values['email'] = $_POST['email'];
+    $values['hash'] = $bcrypt->hash($_POST['pass']);
+    $values['name'] = isset($_POST['name']) ? $_POST['name']:"Virtual MP";
+    $values['riding_lcname'] = $riding;
+    
+    if(isset($_POST['username']))
+      $values['username'] = $_POST['username'];
+    
+    foreach($values as &$v)
+      $v = DB::get(1)->quote($v);
+      
+
+    $fields = implode(",",array_keys($values));
+    $values = implode(",",$values);
+    
+    try {
+    
+      $r = DB::get(1)->query("insert into user ({$fields}) values ({$values})");
+      if($r->rowCount()){
+        $this->response->success = true;
+      } else {
+        return "Unable to add user to database";
+      }
+    } catch(PDOException $e){
+      
+      return json_encode($e);
+      
+    }
+    
+    return false;
+    
+  }
   
+  public function PUT($args){
+    
   
   }
   
+  public function DELETE($args){
 
-
-
+  
+  }
+  
+  
+  
 }
